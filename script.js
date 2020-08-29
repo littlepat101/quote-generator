53efc6cf681dad877ca993929b7af8269530be81
Patrick <patrick2code@gmail.com>
2020-08-28 19:35:46 -0700

    improved code readability

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
 
-// Show Loading
-function loading() {
+function showLoadingSpinner() {
    loader.hidden = false;
    quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
    if (!loader.hidden) {
       quoteContainer.hidden = false;
       loader.hidden = true;
@@ -22,7 +20,7 @@ function complete() {
 
 // Get Quote From API
 async function getQuote() {
-   loading();
+   showLoadingSpinner();
    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
    const apiUrl =
       "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -43,7 +41,7 @@ async function getQuote() {
          authorText.innerText = data.quoteAuthor;
       }
       // Stop Loader, Show Quote
-      complete();
+      removeLoadingSpinner();
    } catch (error) {
       getQuote();
    }
